05134027462938eb67b2ab73aa67265e2da2b7c2
Mr Coxall <mr.coxall@mths.ca>
2018-04-03 21:01:44 -0400

    added level 0

--- levels/level0.lua
+++ levels/level0.lua
@@ -8,119 +8,186 @@
 -- This file is level 0
 -----------------------------------------------------------------------------------------
 
+-- Include modules/libraries
 local composer = require( "composer" )
 local scene = composer.newScene()
+local widget = require( "widget" )
+local physics = require( "physics" )
+local fx = require( "com.ponywolf.ponyfx" )
+local tiled = require( "com.ponywolf.ponytiled" )
+local json = require( "json" )
 
--- include Corona's "physics" library
-local physics = require "physics"
+-- Name this Scene: note this is a Global variable
+sceneName = "level0_scene"
 
---------------------------------------------
+-- Create a new Composer scene
+local scene = composer.newScene( sceneName )
 
--- forward declarations and other locals
-local screenW, screenH, halfW = display.actualContentWidth, display.actualContentHeight, display.contentCenterX
+-- for arranging objects in the new scene
+local centerX = display.contentCenterX
+local centerY = display.contentCenterY
+local screenTop = display.screenOriginY
+local screenLeft = display.screenOriginX
+local screenWidth = display.contentWidth
+local screenHeight = display.contentHeight
 
-function scene:create( event )
-
-	-- Called when the scene's view does not exist.
-	-- 
-	-- INSERT code here to initialize the scene
-	-- e.g. add display objects to 'sceneGroup', add touch listeners, etc.
-
-	local sceneGroup = self.view
-
-	-- We need physics started to add bodies, but we don't want the simulaton
-	-- running until the scene is on the screen.
-	physics.start()
-	physics.pause()
-
-
-	-- create a grey rectangle as the backdrop
-	-- the physical screen will likely be a different shape than our defined content area
-	-- since we are going to position the background from it's top, left corner, draw the
-	-- background at the real top, left corner.
-	local background = display.newRect( display.screenOriginX, display.screenOriginY, screenW, screenH )
-	background.anchorX = 0 
-	background.anchorY = 0
-	background:setFillColor( .5 )
-	
-	-- make a crate (off-screen), position it, and rotate slightly
-	local crate = display.newImageRect( "crate.png", 90, 90 )
-	crate.x, crate.y = 1260, -100
-	crate.rotation = 15
-	
-	-- add physics to the crate
-	physics.addBody( crate, { density=1.0, friction=0.3, bounce=0.3 } )
-	
-	-- create a grass object and add physics (with custom shape)
-	local grass = display.newImageRect( "grass.png", screenW, 82 )
-	grass.anchorX = 0
-	grass.anchorY = 1
-	--  draw the grass at the very bottom of the screen
-	grass.x, grass.y = display.screenOriginX, display.actualContentHeight + display.screenOriginY
-	
-	-- define a shape that's slightly shorter than image bounds (set draw mode to "hybrid" or "debug" to see)
-	local grassShape = { -halfW,-34, halfW,-34, halfW,34, -halfW,34 }
-	physics.addBody( grass, "static", { friction=0.3, shape=grassShape } )
-	
-	-- all display objects must be inserted into group
-	sceneGroup:insert( background )
-	sceneGroup:insert( grass)
-	sceneGroup:insert( crate )
-end
-
-
-function scene:show( event )
-	local sceneGroup = self.view
-	local phase = event.phase
-	
-	if phase == "will" then
-		-- Called when the scene is still off screen and is about to move on screen
-	elseif phase == "did" then
-		-- Called when the scene is now on screen
-		-- 
-		-- INSERT code here to make the scene come alive
-		-- e.g. start timers, begin animation, play audio, etc.
-		physics.start()
-	end
-end
-
-function scene:hide( event )
-	local sceneGroup = self.view
-	
-	local phase = event.phase
-	
-	if event.phase == "will" then
-		-- Called when the scene is on screen and is about to move off screen
-		--
-		-- INSERT code here to pause the scene
-		-- e.g. stop timers, stop animation, unload sounds, etc.)
-		physics.stop()
-	elseif phase == "did" then
-		-- Called when the scene is now off screen
-	end	
-	
-end
-
-function scene:destroy( event )
-
-	-- Called prior to the removal of scene's "view" (sceneGroup)
-	-- 
-	-- INSERT code here to cleanup the scene
-	-- e.g. remove display objects, remove touch listeners, save state, etc.
-	local sceneGroup = self.view
-	
-	package.loaded[physics] = nil
-	physics = nil
-end
-
----------------------------------------------------------------------------------
-
--- Listener setup
-scene:addEventListener( "create", scene )
-scene:addEventListener( "show", scene )
-scene:addEventListener( "hide", scene )
-scene:addEventListener( "destroy", scene )
+-- Are we running on the Corona Simulator?
+-- https://docs.coronalabs.com/api/library/system/getInfo.html
+local isSimulator = "simulator" == system.getInfo( "environment" )
+local isMobile = ( "ios" == system.getInfo("platform") ) or ( "android" == system.getInfo("platform") )
 
 -----------------------------------------------------------------------------------------
+-- Place your scene spacific code below
+-----------------------------------------------------------------------------------------
+
+-- Variables Global to the scene; note make sure you do not declare with "local"
+--
+
+-- This module turns gamepad axis events and mobile accelerometer events into keyboard
+-- events so we don't have to write separate code for joystick, tilt, and keyboard control
+require( "com.ponywolf.joykey" ).start()
+
+-- add virtual joysticks to mobile 
+system.activate("multitouch")
+if isMobile or isSimulator then
+	local vjoy = require( "com.ponywolf.vjoy" )
+	local stick = vjoy.newStick()
+	stick.x, stick.y = 128, display.contentHeight - 128
+	local button = vjoy.newButton()
+	button.x, button.y = display.contentWidth - 128, display.contentHeight - 128
+end
+
+-- Variables local to scene
+local map, hero, shield, parallax
+
+-- Functions local to scene, like event handlers for buttons
+local function menuTransition( )
+	--composer.gotoScene("scene.splashScene")
+
+end
+
+-- This function is called when scene is created
+function scene:create( event )
+
+	local sceneGroup = self.view  -- Add scene display objects to this group
+
+	-- Display background
+	--local background = display.newRect( centerX, centerY, screenWidth ,screenHeight )
+	--background:setFillColor( 0, 0, 255 ) 
+
+	-- Associating display objects with this scene
+	--sceneGroup:insert( background )
+
+	-- stream music
+	--bgMusic = audio.loadStream( "scene/menu/sfx/titletheme.mp3" )
+
+	-- Start physics before loading map
+	physics.start()
+	physics.setGravity( 0, 32 )
+
+	-- Load our map
+	--local filename = "./assets/maps/winterScene.json"
+	--local mapData = json.decodeFile( system.pathForFile( filename, system.ResourceDirectory ) )
+	
+	local filename = "./assets/maps/level0.json"
+	local mapData = json.decodeFile( system.pathForFile( filename, system.ResourceDirectory ) )
+	--map = tiled.new( mapData, "./assets/maps" )	
+	map = tiled.new( mapData, "./assets/sprites" )
+
+	-- drag the whole map for fun
+	local dragable = require "com.ponywolf.plugins.dragable"
+	map = dragable.new(map)
+
+	-- Find our hero!
+	--map.extensions = "assets.lib."
+	--map:extend( "hero" )
+	--hero = map:findObject( "hero" )
+	--hero.filename = filename
+
+	-- Find our enemies and other items
+	--map:extend( "blob", "enemy", "exit", "coin", "spikes" )
+
+	-- Find the parallax layer
+	--parallax = map:findLayer( "parallax" )
+
+			-- Add our scoring module
+	--local gem = display.newImageRect( sceneGroup, "./assets/lib/img/gem.png", 64, 64 )
+	--gem.x = display.contentWidth - gem.contentWidth / 2 - 24
+	--gem.y = display.screenOriginY + gem.contentHeight / 2 + 20
+
+
+
+	-- Insert our game items in the correct back-to-front order
+	sceneGroup:insert( map )
+
+	--sceneGroup:insert( gem )
+
+
+end
+
+-- Function to scroll the map
+local function enterFrame( event )
+
+	local elapsed = event.time
+
+	-- Easy way to scroll a map based on a character
+	--if hero and hero.x and hero.y and not hero.isDead then
+	--	local x, y = hero:localToContent( 0, 0 )
+	--	x, y = display.contentCenterX - x, display.contentCenterY - y
+	--	map.x, map.y = map.x + x, map.y + y
+		-- Easy parallax
+	--	if parallax then
+	--		parallax.x, parallax.y = map.x / 6, map.y / 8  -- Affects x more than y
+	--	end
+	--end
+end
+
+--local function enterFrame( event )
+
+--	local elapsed = event.time
+
+--end
+
+-- This function is called when scene comes fully on screen
+function scene:show( event )
+
+	local phase = event.phase
+	if ( phase == "will" ) then
+		Runtime:addEventListener( "enterFrame", enterFrame )
+	elseif ( phase == "did" ) then
+
+	end
+
+end
+
+-- This function is called when scene goes fully off screen
+function scene:hide( event )
+
+	local phase = event.phase
+	if ( phase == "will" ) then
+
+	elseif ( phase == "did" ) then
+		Runtime:removeEventListener( "enterFrame", enterFrame )
+	end
+
+end
+
+-- This function is called when scene is destroyed
+function scene:destroy( event )
+
+	audio.stop()  -- Stop all audio
+	for s, v in pairs( self.sounds ) do  -- Release all audio handles
+		audio.dispose( v )
+		self.sounds[s] = nil
+	end
+
+  --collectgarbage()
+
+end
+
+scene:addEventListener( "create" )
+scene:addEventListener( "show" )
+scene:addEventListener( "hide" )
+scene:addEventListener( "destroy" )
 
 return scene
